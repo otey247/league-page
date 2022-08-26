52a3f0dda6e9cebc18404cb8ca9e34ebff3d3bfe
otey247 <50851804+otey247@users.noreply.github.com>
2022-08-26 10:37:35 -0500

    Add files via upload

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -125,7 +125,7 @@
 		</ul>
 	</div>
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/otey247/league-page">League Page</a></span>
+	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/otey247/league-page">Lots of Shots League Page</a></span>
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
 	<span class="creator">Built by <a href="http://www.devourdata.com/">Jo Otey</a><br /></span>
