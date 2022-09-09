9c83c9a9188c128a4c7cbbbd0c3982af34339276
Jo Otey <otey247@gmail.com>
2022-09-09 09:14:12 -0500

    Add Tables and Images

--- src/routes/api/addBlogComments/[id].js
+++ src/routes/api/addBlogComments/[id].js
@@ -1,8 +1,8 @@
-import * as contentfulManagement from 'contentful-management';
+import contentful from 'contentful-management'
 
 import { getLeagueUsers } from "$lib/utils/helper";
 
-const client = contentfulManagement.createClient({
+const client = contentful.createClient({
     // This is the access token for this space. Normally you get the token in the Contentful web app
     accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
 });
